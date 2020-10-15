fff8ba75939700e148afa3401e4c486710ca3fc6
zzc20 <z18351828988@163.com>
2020-10-15 11:27:47 +0800

    'navbar,homecomps,homerecommends'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,27 @@
 <template>
-  <NavBar><div slot="center">购物车</div></NavBar>
+  <div id="home">
+    <NavBar><div slot="center">购物车</div></NavBar>
+    <HomeComps :banners="banners"/>
+    <HomeRecommend :recommends="recommends"/>
+  </div>
+
+
+
+
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import HomeComps from "./childcomps/HomeComps";
+  import HomeRecommend from "./childcomps/HomeRecommend";
+
   import {getHomeMultidata} from "network/home"
   export default {
     name: "Home",
     components : {
       NavBar,
+      HomeComps,
+      HomeRecommend
     },
     data() {
       return {
@@ -18,7 +31,7 @@
     },
     created() {
       getHomeMultidata().then(res => {
-        console.log(res);
+        // console.log(res);
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
 
---
+++ src/views/home/childcomps/HomeRecommend.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+          <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "HomeRecommend",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+
+  }
+  .recommend-item {
+    flex: 1;
+    text-align: center;
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee;
+  }
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 8px;
+
+  }
+
+</style>
